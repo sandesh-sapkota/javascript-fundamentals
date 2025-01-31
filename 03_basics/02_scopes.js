93b9be0284517fddea7b9b0b557b0258a7247726
sandesh-sapkota <sapkotasandeshlive12@gmail.com>
2025-01-31 23:27:25 +0545

    Scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,25 @@
+
+if (true) {
+  let a = 10;
+  const b = 20;
+  var c = 30;
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username="sandy";
+    
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+
+one();
