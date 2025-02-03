182670d1dfb533428ff0d03615ae0abd8c754a20
sandesh-sapkota <sapkotasandeshlive12@gmail.com>
2025-02-03 21:12:57 +0545

    while and do while loop

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,23 @@
+//***while loop */
+
+// let i=0;
+//     while (i<=10) {
+//         console.log(`value of index is: ${i}`);
+//         i++;
+//     }
+
+    // let myArray=['flash','batman','superman'];
+
+    // let i=0;
+    // while (i<myArray.length) {
+    //     console.log(`value is : ${myArray[i]}`);
+    //     i++;
+    // }
+
+//***do while loop */
+
+let score=1;
+do {
+    console.log(`Score is: ${score}`);
+    score++;
+} while (score<=10);
