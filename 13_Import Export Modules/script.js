29d7e0ecb90b6fe264ab8b406d32f6fe18911a4c
sandesh-sapkota <sapkotasandeshlive12@gmail.com>
2025-02-17 08:58:21 +0545

    ES6 Import/Export Modules

---
+++ 13_Import Export Modules/script.js
@@ -0,0 +1,13 @@
+// import { mul,add } from "./utility.js";
+import * as utility  from "./utility.js";
+// console.log(`Multiply is: ${multiply(2,3,4)}`);
+
+
+ const result= utility.mul(1,2,3);
+ 
+ const result1= utility.add(4,2,3);
+
+ console.log('Multiply:' , result);
+
+ console.log('Addition:', result1);
+ 
---
+++ 13_Import Export Modules/utility.js
@@ -0,0 +1,12 @@
+ function multiply(a,b,c){
+    return a*b*c;
+}
+
+function addition(a,b,c){
+    return a+b+c;
+}
+
+export{
+    multiply as mul,
+    addition as add
+}
